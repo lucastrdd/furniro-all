b87236d4ffcfd2e0314438f12d974610de1a0826
Lucas Trindade <lucasmedeirostrindade@gmail.com>
2026-08-25 18:36:17 -0300

    refactor(header): simplify fixed header structure

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,40 +1,38 @@
 import clsx from "clsx";
+import { Link } from "react-router-dom";
 import Logo from "../Logo";
 import MobileMenu from "../MobileMenu";
 import NavMenu from "../NavMenu";
 import RightMenu from "../RigthMenu";
-import { Link } from "react-router-dom";
 
 const Header = () => {
-  return (
-    <div>
-      <div
-        className={clsx(
-          "flex justify-center items-center",
-          "h-25 w-full max-w-360",
-          "fixed z-50",
-          "bg-primary",
-        )}
-      >
-        <div
-          className={clsx(
-            "flex justify-between items-center",
-            "w-full px-2 max-h-10.25",
-            "md:px-4",
-            "lg:px-12.5",
-          )}
-        >
-          <Link to={"/"}>
-            <Logo></Logo>
-          </Link>
-          <NavMenu className={clsx("hidden", "md:flex")}></NavMenu>
-          <RightMenu className={clsx("hidden", "md:flex")}></RightMenu>
-          <MobileMenu className={clsx("flex", "md:hidden")}></MobileMenu>
-        </div>
-      </div>
-      {/*Essa div serve apenas como elemento para preencher o espaço do fixed*/}
-      <div className={clsx("h-25", "-z-50")}></div>
-    </div>
-  );
+    return (
+        <>
+            <header
+                className={clsx(
+                    "fixed inset-x-0 top-0 z-50",
+                    "flex h-25 w-full items-center justify-center",
+                    "bg-primary",
+                )}>
+                <div
+                    className={clsx(
+                        "flex max-h-10.25 w-full max-w-360 items-center justify-between px-2",
+                        "md:px-4",
+                        "lg:px-12.5",
+                    )}>
+                    <Link to="/" aria-label="Furniro home">
+                        <Logo />
+                    </Link>
+
+                    <NavMenu className={clsx("hidden", "md:flex")} />
+                    <RightMenu className={clsx("hidden", "md:flex")} />
+                    <MobileMenu className={clsx("flex", "md:hidden")} />
+                </div>
+            </header>
+
+            <div className="h-25" aria-hidden="true" />
+        </>
+    );
 };
+
 export default Header;
