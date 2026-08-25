bcaf86d5e52ad518d2a4e1e1cc4d31e9234f8043
Lucas Trindade <lucasmedeirostrindade@gmail.com>
2026-08-25 10:34:44 -0300

    feat(auth): build registration page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,30 +3,39 @@ import Container from "./components/Container";
 import Header from "./components/Header";
 import Home from "./pages/Home/page";
 import Footer from "./components/Footer";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Product from "./pages/Product/page";
 import Shop from "./pages/Shop/page";
 import Cart from "./pages/Cart/page";
+import Register from "./pages/Register/page";
 import NotFoundPage from "./pages/NotFoundPage";
 
 const App = () => {
+    const { pathname } = useLocation();
+    const isAuthPage = pathname === "/register";
+
     return (
         <>
             <Toaster />
 
-            <Container className="bg-[#FFF]">
-                <Header />
-            </Container>
+            {!isAuthPage && (
+                <Container className="bg-[#FFF]">
+                    <Header />
+                </Container>
+            )}
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/shop/:category?" element={<Shop />} />
                 <Route path="/product/:slug" element={<Product />} />
                 <Route path="/cart" element={<Cart />} />
+                <Route path="/register" element={<Register />} />
                 <Route path="*" element={<NotFoundPage />} />
             </Routes>
-            <Container className="bg-primary border-t border-t-[rgba(0,0,0,0.17)]">
-                <Footer />
-            </Container>
+            {!isAuthPage && (
+                <Container className="bg-primary border-t border-t-[rgba(0,0,0,0.17)]">
+                    <Footer />
+                </Container>
+            )}
         </>
     );
 };
